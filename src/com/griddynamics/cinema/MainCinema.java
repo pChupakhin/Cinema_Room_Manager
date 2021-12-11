536383af03810692643e025151f39217679c855e
pChupakhin <pchupakhin@griddynmics.com>
2021-12-11 08:09:48 -0800

    CHANGES FROM THE LAST PULL REQUEST  1. Cinema, MainCinema, Menu classes created  to avoid non-conventional static  2. All unnecessary static removed  3. All classes packed in com.driddynamics.cinema  4. .gitignore updated

---
+++ src/com/griddynamics/cinema/MainCinema.java
@@ -0,0 +1,7 @@
+package com.griddynamics.cinema;
+
+public class MainCinema {
+        public static void main(String[] args) {
+        new Menu().cinemaMenu();
+    }
+}
---
+++ src/com/griddynamics/cinema/Menu.java
@@ -0,0 +1,71 @@
+package com.griddynamics.cinema;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	private static final Scanner scanner =  new Scanner(System.in);
+	
+	private static final Cinema cinema;
+	
+	static {
+		
+		System.out.print("Enter the number of rows:\n> ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of seats in each row:\n> ");
+        int seatsInEachRow = scanner.nextInt();
+        
+        cinema = new Cinema(rows, seatsInEachRow);
+        
+	}
+	
+	
+	
+	private void ticketPurchasingMenu() {
+    	
+        int row;
+        int seat;
+        String errorMessage;
+    	
+        do {
+
+            System.out.print("\nEnter a row number:\n> ");
+            row = scanner.nextInt() - 1;
+            
+            System.out.print("Enter a seat number in that row:\n> ");
+            seat = scanner.nextInt() - 1;
+            
+            errorMessage = row >= cinema.ROWS || seat >= cinema.SEATS_IN_EACH_ROW
+            		? "Wrong input!\n" : cinema.HALL[row][seat] == 'B'
+                    ? "That ticket has already been purchased!\n" : "";
+
+            System.out.print('\n' + errorMessage);
+
+        } while(!errorMessage.isEmpty());
+        
+        System.out.println("Ticket price: $" + cinema.buyTicket(row, seat));
+        
+	}
+	
+	
+	
+	public void cinemaMenu() {
+		
+		int choice;
+
+        do {
+
+            System.out.print("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit\n> ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: cinema.showSeats(); break;
+                case 2: this.ticketPurchasingMenu(); break;
+                case 3: cinema.statistics(); break;
+            }
+
+        } while(choice != 0);
+        
+	}
+
+}
